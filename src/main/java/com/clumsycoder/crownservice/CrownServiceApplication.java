796aec0d4937a713123eea5dedc1a5c87aa43b22
Gaurav Aggarwal <gauravxor@gmail.com>
2025-06-03 02:06:03 +0530

    Initial Commit

---
+++ src/main/java/com/clumsycoder/crownservice/CrownServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.crownservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrownServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrownServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clumsycoder/crownservice/CrownServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.crownservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrownServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
